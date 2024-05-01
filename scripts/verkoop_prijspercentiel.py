878a1a04172d899f9cf91713d36e56cf53246df0
timsideanotmine <86826343+timsideanotmine@users.noreply.github.com>
2024-05-01 17:07:28 +0200

    herbekijken van grafieken in het productie model (deel1) , module maken van het prijs/rang percentiel voor deel 2 van de opgave (verkoop auto's aka volvo's), herwerken van vraag 1, 2 en 3

---
+++ scripts/verkoop_prijspercentiel.py
@@ -0,0 +1,44 @@
+# -------------------------------------------------------------
+# standaard in python
+import os 
+import datetime
+import sys
+import random
+import calendar
+import gzip
+import json
+
+# -------------------------------------------------------------
+# te installeren packages en modules
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+
+from scipy.stats import weibull_min
+from scipy.stats import skewnorm
+from scipy.stats import norm
+from scipy.stats import pareto
+from scipy.stats import beta
+
+from sklearn.linear_model import LinearRegression
+
+from tqdm import tqdm
+
+import seaborn as sns
+
+def bereken_percentiel_voor_rang(prijs_rang, rijen):
+    """
+    bereken in welk percentieel de prijs van een merk ligt.
+
+    Args:
+        prijs_rang (int): de rang in de gesorteerde dataframe
+        rijen (int): totaal_aantal rijen van de dataframe 
+
+    Returns:
+        float: het percentage percentiel
+    """
+    percentiel = (prijs_rang / rijen) * 100
+    return percentiel
